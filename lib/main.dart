c0257f3e382527082f1e20ce953ee40326651ccc
Shubham-Emeritus <shubham.bayas@emeritus.org>
2025-07-08 13:22:33 +0530

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'features/add_activity/presentation/screens/add_activity_screen.dart';
+import 'core/theme.dart';
+import 'features/auth/presentation/screens/login_screen.dart';
+import 'features/dashboard/presentation/screens/dashboard_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: DailyWellnessApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DailyWellnessApp extends StatelessWidget {
+  const DailyWellnessApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'DailyWellness',
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+        routes: {
+          '/': (_) => const LoginScreen(),
+          '/dashboard': (_) => const DashboardScreen(),
+          '/add': (_) => const AddActivityScreen(),
+        }
+    );
   }
 }
